9eac2d7da7db17ba91a4de76d691694c81f08589
Leonardo Bravo <leonardbravoillanes@gmail.com>
2015-03-10 18:28:21 -0300

    Pruebas de ejecución

---
+++ oo_method.tests/src/oo_method/tests/Oo_methodAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package oo_method.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Oo_method</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class Oo_methodAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new Oo_methodAllTests("Oo_method Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Oo_methodAllTests(String name) {
+		super(name);
+	}
+
+} //Oo_methodAllTests
---
+++ oo_method.tests/src/oo_method/tests/StructuralModelTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package oo_method.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import oo_method.Oo_methodFactory;
+import oo_method.StructuralModel;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Structural Model</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class StructuralModelTest extends TestCase {
+
+	/**
+	 * The fixture for this Structural Model test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected StructuralModel fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(StructuralModelTest.class);
+	}
+
+	/**
+	 * Constructs a new Structural Model test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public StructuralModelTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Structural Model test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(StructuralModel fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Structural Model test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected StructuralModel getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(Oo_methodFactory.eINSTANCE.createStructuralModel());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //StructuralModelTest
