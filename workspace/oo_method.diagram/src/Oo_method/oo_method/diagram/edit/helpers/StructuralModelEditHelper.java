1f7e0c8795f83a1746771975ba155645085013a4
Leonardo Bravo <leonardbravoillanes@gmail.com>
2015-03-16 16:46:25 -0300

    Modelo oo_method

--- workspace/oo_method.diagram/src/Oo_method/oo_method/diagram/edit/helpers/StructuralModelEditHelper.java
+++
@@ -1,8 +0,0 @@
-package Oo_method.oo_method.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class StructuralModelEditHelper extends
-		Oo_method.oo_method.diagram.edit.helpers.Oo_methodBaseEditHelper {
-}
--- workspace/oo_method.diagram/src/oo_method/diagram/edit/helpers/ClassEditHelper.java
+++
@@ -1,8 +0,0 @@
-package oo_method.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ClassEditHelper extends
-		oo_method.diagram.edit.helpers.Oo_methodBaseEditHelper {
-}
