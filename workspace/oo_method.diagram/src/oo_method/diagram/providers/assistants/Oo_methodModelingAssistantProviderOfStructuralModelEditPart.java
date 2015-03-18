08506fd554a68ee0facdab92204cb95c60d5447c
Leonardo Bravo <leonardbravoillanes@gmail.com>
2015-03-18 18:37:35 -0300

    Clean - Del Antiguos constructores

--- workspace/oo_method.diagram/src/oo_method/diagram/providers/assistants/Oo_methodModelingAssistantProviderOfStructuralModelEditPart.java
+++
@@ -1,9 +0,0 @@
-package oo_method.diagram.providers.assistants;
-
-/**
- * @generated
- */
-public class Oo_methodModelingAssistantProviderOfStructuralModelEditPart extends
-		oo_method.diagram.providers.Oo_methodModelingAssistantProvider {
-
-}
