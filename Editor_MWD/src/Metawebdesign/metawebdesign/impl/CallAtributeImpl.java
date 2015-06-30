6b155ea22681d71f254a9beaa4fb2564a75c9af9
Leonardo Bravo Illanes <leo@olidata>
2015-06-29 21:48:09 -0300

    Fix CallAttribute typepresentationDefault

--- Editor_MWD/src/Metawebdesign/metawebdesign/impl/CallAtributeImpl.java
+++ Editor_MWD/src/Metawebdesign/metawebdesign/impl/CallAtributeImpl.java
@@ -51,7 +51,7 @@ public class CallAtributeImpl extends ViewComponentImpl implements CallAtribute 
 	 * @generated
 	 * @ordered
 	 */
-	protected static final typePresentation TYPE_PRESENTATION_EDEFAULT = typePresentation.STRING;
+	protected static final typePresentation TYPE_PRESENTATION_EDEFAULT = null;
 
 	/**
 	 * The cached value of the '{@link #getTypePresentation() <em>Type Presentation</em>}' attribute.
