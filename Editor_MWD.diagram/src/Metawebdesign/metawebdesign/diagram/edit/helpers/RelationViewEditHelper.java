174976b850d506038aad6df19c2edf7ec37b325b
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-08-27 13:23:13 -0300

    Update model - nueva forma de relacionar clases, para la generación de llaves foraneas

--- Editor_MWD.diagram/src/Metawebdesign/metawebdesign/diagram/edit/helpers/RelationViewEditHelper.java
+++
@@ -1,9 +0,0 @@
-package Metawebdesign.metawebdesign.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class RelationViewEditHelper
-		extends
-		Metawebdesign.metawebdesign.diagram.edit.helpers.MetaWebDesignBaseEditHelper {
-}
