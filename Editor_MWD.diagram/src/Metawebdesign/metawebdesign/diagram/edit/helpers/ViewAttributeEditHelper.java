71bd7061c11476f7099229b50be0c7730fff0945
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-09-25 15:34:33 -0300

    Clean Old version

--- Editor_MWD.diagram/src/Metawebdesign/metawebdesign/diagram/edit/helpers/ViewAttributeEditHelper.java
+++
@@ -1,9 +0,0 @@
-package Metawebdesign.metawebdesign.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ViewAttributeEditHelper
-		extends
-		Metawebdesign.metawebdesign.diagram.edit.helpers.MetaWebDesignBaseEditHelper {
-}
