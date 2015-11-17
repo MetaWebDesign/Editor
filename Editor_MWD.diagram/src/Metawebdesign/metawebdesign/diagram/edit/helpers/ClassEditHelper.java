c5f8cbdb4124b469faf7ff0beef819ac2a64c9f1
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-11-17 17:22:33 -0300

    Clean versión 0.2.7

--- Editor_MWD.diagram/src/Metawebdesign/metawebdesign/diagram/edit/helpers/ClassEditHelper.java
+++
@@ -1,9 +0,0 @@
-package Metawebdesign.metawebdesign.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ClassEditHelper
-		extends
-		Metawebdesign.metawebdesign.diagram.edit.helpers.MetaWebDesignBaseEditHelper {
-}
